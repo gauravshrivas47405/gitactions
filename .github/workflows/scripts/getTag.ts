02d7c5e5354e2cc61ec2fd6650ac3a1f07d958c2
Gaurav <gaurav.shrivas@zensar.com>
2023-10-26 00:22:48 +0530

    Tag WIP

--- .github/workflows/scripts/getTag.ts
+++ .github/workflows/scripts/getTag.ts
@@ -7,13 +7,14 @@ const main = async () => {
 try {
     // `who-to-greet` input defined in action metadata file
     const gitTags = core.getInput('gitTags');
-    console.log(`gittags========>>> ${gitTags}!`);
+    console.log(`gittags========>>> ${gitTags}`);
     let newTag = 1;
     
     if(gitTags.includes('_rc')){
       let previousIndex = gitTags.split('_rc')[1];
        newTag = await parseInt(previousIndex)+1;
     }
+    console.log(`newTag========>>> ${newTag}`);
     core.setOutput("newTag", newTag);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
