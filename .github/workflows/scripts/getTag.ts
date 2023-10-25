01054584b1e22584a90d32dee11004ffd51b0d26
Gaurav <gaurav.shrivas@zensar.com>
2023-10-25 18:47:05 +0530

    Tag WIP

---
+++ .github/workflows/scripts/getTag.ts
@@ -0,0 +1,18 @@
+
+
+import core from '@actions/core';
+import github from '@actions/github';
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const gitTags = core.getInput('gitTags');
+    console.log(`gittags========>>> ${gitTags}!`);
+    let previousIndex = gitTags.split('_rc')[1];
+    let newIndex = parseInt(previousIndex)+1;
+    core.setOutput("newIndex =======>>>", newIndex);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
