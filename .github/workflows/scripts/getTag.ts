cdd8e835e0eb4b6edf9de74ed75d1760f6fcba16
Gaurav <gaurav.shrivas@zensar.com>
2023-10-26 00:32:25 +0530

    Tag WIP

--- .github/workflows/scripts/getTag.ts
+++ .github/workflows/scripts/getTag.ts
@@ -17,8 +17,8 @@ try {
     console.log(`newTag========>>> ${newTag}`);
     core.setOutput("newTag", newTag);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
