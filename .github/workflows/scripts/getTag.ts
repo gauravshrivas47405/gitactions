7e6b43e0733edea0ccb9f80f5cbcdf49808565c7
Gaurav <gaurav.shrivas@zensar.com>
2023-10-26 10:40:00 +0530

    Tag WIP

--- .github/workflows/scripts/getTag.ts
+++ .github/workflows/scripts/getTag.ts
@@ -16,7 +16,10 @@ try {
        newTag = parseInt(previousTag)+1;
     }
     console.log(`newTag========>>> ${newTag}`);
-    core.setOutput("incrementeValue", newTag);
+    core.setOutput("newTag", newTag);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
