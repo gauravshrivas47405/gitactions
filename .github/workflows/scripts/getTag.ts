2aac9923a7c1ae8c4f882bef7f818e0cb0c96a81
Gaurav <gaurav.shrivas@zensar.com>
2023-10-26 12:14:35 +0530

    Tag WIP

--- .github/workflows/scripts/getTag.ts
+++ .github/workflows/scripts/getTag.ts
@@ -1,26 +1,18 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const main = () => {
+const getIncrementedTagValue = () => {
   try {
-    // `who-to-greet` input defined in action metadata file
     const currentTag = core.getInput("currentTag");
-    console.log(`gittags========>>> ${currentTag}`);
     let nextTag = 1;
-
     if (currentTag.includes("_rc")) {
       let previousIndex = currentTag.split("_rc")[1];
-      console.log("previousIndex=======>>", previousIndex);
       nextTag = parseInt(previousIndex) + 1;
     }
-    console.log(`nextTag========>>> ${nextTag}`);
     core.setOutput("nextTag", nextTag);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
 };
 
-main();
+getIncrementedTagValue();
