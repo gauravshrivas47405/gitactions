5bad134a75626e788f8cfd4036241904cd91a552
Gaurav <gaurav.shrivas@zensar.com>
2023-10-26 11:34:13 +0530

    Tag WIP

--- .github/workflows/scripts/getTag.ts
+++ .github/workflows/scripts/getTag.ts
@@ -3,20 +3,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const getIncrementedTag =() => {
+const main = async () => {
 try {
     // `who-to-greet` input defined in action metadata file
-    const currentTag = core.getInput('currentTag');
-    console.log(`currentTag========>>> ${currentTag}`);
-    let nextTag = 1;
+    const gitTags = core.getInput('gitTags');
+    console.log(`gittags========>>> ${gitTags}`);
+    let newTag = 1;
     
-    if(currentTag.includes('_rc')){
-      let previousTag = currentTag.split('_rc')[1];
-      console.log(`previousTag==========>>>${previousTag}`);
-      nextTag = parseInt(previousTag)+1;
+    if(gitTags.includes('_rc')){
+      let previousIndex = gitTags.split('_rc')[1];
+      console.log()
+       newTag = await parseInt(previousIndex)+1;
     }
-    console.log(`nextTag========>>> ${nextTag}`);
-    core.setOutput("nextTag", nextTag);
+    console.log(`newTag========>>> ${newTag}`);
+    core.setOutput("newTag", newTag);
     // Get the JSON webhook payload for the event that triggered the workflow
     // const payload = JSON.stringify(github.context.payload, undefined, 2)
     // console.log(`The event payload: ${payload}`);
@@ -25,4 +25,4 @@ try {
   }
 }
 
-getIncrementedTag();
+main();
