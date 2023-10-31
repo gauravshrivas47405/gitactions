6446ca98c21ab374ecceca4650b65b61a0c09417
Gaurav <gaurav.shrivas@zensar.com>
2023-10-31 14:48:05 +0530

    Tag WIP

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js 1</Text>
+      <Text>Checking if workfloe runs or not!</Text>
       <StatusBar style="auto" />
     </View>
   );
