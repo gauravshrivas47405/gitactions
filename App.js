0d612436961ec19d462e0ff88856a17554c9e9cf
Gaurav <gaurav.shrivas@zensar.com>
2023-10-31 21:09:32 +0530

    Feat/app-32 -1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Working on APP 32 -1</Text>
+      <Text>Working on APP 32 -2</Text>
       <StatusBar style="auto" />
     </View>
   );
