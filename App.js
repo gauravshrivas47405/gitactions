44d00015be3e32ef82669a8385610160e3c9357d
Gaurav <gaurav.shrivas@zensar.com>
2023-10-31 21:05:01 +0530

    Tag WIP

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js 1</Text>
-      <Text>Open up App.js !</Text>
+      <Text>Working on APP 32 -1</Text>
       <StatusBar style="auto" />
     </View>
   );
