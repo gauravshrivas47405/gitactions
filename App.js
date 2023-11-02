206baa57f488965c27eda363e044b920862397fc
Gaurav <gaurav.shrivas@zensar.com>
2023-11-02 13:32:27 +0530

    Feat/app-32 App.js file changed

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Working on APP 32 -46</Text>
       <StatusBar style="auto" />
     </View>
   );
