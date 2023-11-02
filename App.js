d9995b53b1df6632cfad24cc9c6ed5cc7fbe5115
Gaurav <gaurav.shrivas@zensar.com>
2023-11-02 14:08:29 +0530

    Feat/app-32 App.js file changed

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Setup the Cherry-pick PR Process for RC bug fixes</Text>
       <StatusBar style="auto" />
     </View>
   );
